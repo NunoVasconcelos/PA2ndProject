0e3209477d50992045be8c68f7348642110e1ed0
Nuno Vasconcelos <nunorogerio12@hotmail.com>
2016-04-28 11:16:04 +0100

    Test added

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import ist.meic.pa.GenericFunctions.GenericFunction;
+import ist.meic.pa.GenericFunctions.GFMethod;
+
+
+public class Test {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
